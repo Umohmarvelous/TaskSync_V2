041b943fedc5427a0b858f64a7e5f4f475c2f1cf
Umoh Marvelous <umohm450@gmail.com>
2025-07-02 11:56:40 +0100

    fixed path error

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import { Sidebar } from "@/components/side-bar-component"
-import { DashboardHeader } from "@/components/dashboard-header-component"
-import { HomeContent } from "@/components/home-component"
-import { ForgotPassword } from "@/components/forgot-password-component"
-import { UserTypeSelection } from "@/components/user-type-selection"
 import Welcome from "./welcome/page"
 
 export default function HomePage() {
   return (
-    // <div className="min-h-auto bg-gray-50">
-    //   <Sidebar />
-    //   <div className="ml-64">
-    //     <DashboardHeader/>
-    //     <main className="pt-16">
-    //       <HomeContent />
-    //     </main>
-    //   </div>
-    // </div>
     <div>
       <Welcome />
     </div>
