2388ab979845aef15ac97f156b84da4178093afd
Umoh Marvelous <umohm450@gmail.com>
2025-07-14 22:30:45 +0100

    updated

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -13,7 +13,7 @@ export default function LogoutLayout() {
             {showBackButton && (
 
                 <Button
-                    variant={"outline"} className="w-full mt-3 justify-start text-slate-300 hover:text-white hover:bg-slate-700"
+                    variant={"ghost"} className="w-full mt-3 justify-start text-slate-300 hover:text-white hover:bg-slate-700"
                     onClick={() => router.back()}>
                     <LogOut className="w-5 h-5 mr-3" />
 
