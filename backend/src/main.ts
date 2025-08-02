95ce25e3ed86e05617bf58b4f01e17f8107353a0
Umoh Marvelous <umohm450@gmail.com>
2025-08-02 07:23:30 +0100

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  await app.init();
 }
 bootstrap();
+
+export default server;
